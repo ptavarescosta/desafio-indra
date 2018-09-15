731b74d955500580abe968b6c2e497fc9f48cd52
oAzzi <razzi@indracompany.com>
2018-09-15 02:20:15 -0300

    Primeira App Java para a imagem do Docker

---
+++ eclipse-workspace/greenhat/src/main/java/greenhat/App1.java
@@ -0,0 +1,11 @@
+package greenhat;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+}
